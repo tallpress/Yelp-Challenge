a98c04bc4096b129a37e773aeb383c9535803541
tom <tomallpress@hotmail.com>
2018-01-15 17:47:52 +0000

    added restuarant display list

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,10 +1,27 @@
 class RestaurantsController < ApplicationController
 
+  def index
+    @restaurants = Restaurant.all
+  end
+  
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
   def new
   end
 
   def create
-    render plain: params[:restaurant].inspect
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+    redirect_to @restaurant
+
+  end
+
+  private
+  def restaurant_params
+    params.require(:restaurant).permit(:restaurant_name, :restaurant_cuisine,
+      :restaurant_price, :restaurant_address)
   end
 
 end
