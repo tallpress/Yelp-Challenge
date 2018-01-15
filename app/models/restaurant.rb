d86c3ca660a974aae5e73cfa5b023b934ee51007
tom <tomallpress@hotmail.com>
2018-01-15 18:07:04 +0000

    added some validations for the restaurants

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,7 @@
 class Restaurant < ApplicationRecord
-  
+  validates :restaurant_name, presence: true,
+                    length: {minimum:5, maximum: 50}
+  validates :restaurant_price, presence: true
+  validates :restaurant_cuisine, presence: true
+  validates :restaurant_address, presence: true
 end
