1c29fe02b5e3549880816d16d483418f3bd12b4e
tom <tomallpress@hotmail.com>
2018-01-16 16:54:16 +0000

    added average rating feature

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,4 +5,11 @@ class Restaurant < ApplicationRecord
   validates :restaurant_address, presence: true
 
   has_many :reviews, dependent: :destroy
+
+  def average_rating
+    @sum = 0
+    self.reviews.each { |review| @sum += review.rating }
+    (@sum.to_f/ self.reviews.count).round(1)
+  end
+
 end
