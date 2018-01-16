6c191cbb69e7a385445060fc8a7c5a2feb314dfc
Lars <larsfinlay@googlemail.com>
2018-01-16 08:09:50 +0000

    Added some more tests and made update

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -12,12 +12,26 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.new
   end
 
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
   def create
     @restaurant = Restaurant.new(restaurant_params)
     if @restaurant.save
       redirect_to @restaurant
     else
       render 'new'
+    end
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+
+    if @restaurant.update(restaurant_params)
+      redirect_to @restaurant
+    else
+      render 'edit'
     end
   end
 
--- spec/restaurant/adding_restaurant_spec.rb
+++ spec/restaurant/adding_restaurant_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper.rb'
+require 'web_helper.rb'
 
 feature 'Creating restaurant' do
 
@@ -6,36 +7,57 @@ feature 'Creating restaurant' do
     visit('restaurants/')
   end
 
+  scenario 'test restaurant table is being cleaned after each test suite' do
+    expect(page).not_to have_content("Boyce's beans")
+  end
+
   scenario 'can create a restaurant with right details' do
     click_link 'Add restaurant'
-    fill_in 'restaurant[restaurant_name]', with: "Boyce's beans"
-    fill_in 'restaurant[restaurant_cuisine]', with: "Bean based foods"
-    fill_in 'restaurant[restaurant_price]', with: "3"
-    fill_in 'restaurant[restaurant_address]', with: "Waterloo Station"
-    click_button 'Save Restaurant'
+    create_restaurant("Boyce's beans", "Bean based foods", "3", "Waterloo Station")
     expect(page).to have_content("Boyce's beans")
+  end
+
+  xscenario 'can view restaurant created on restaurants/ page' do
+    click_link 'Add restaurant'
+    create_restaurant("Mcdonalds", "Fast food", "1", "Waterloo Station")
+    visit('restaurants/')
+    expect(page).to have_content("Mcdonalds")
+  end
+
+  scenario 'test restaurant table is being cleaned after each test' do
+    expect(page).not_to have_content("Boyce's beans")
   end
 
   scenario "can't save a restaurant with wrong details" do
     click_link 'Add restaurant'
-    fill_in 'restaurant[restaurant_name]', with: "Arnold's Chicken Hut"
-    fill_in 'restaurant[restaurant_cuisine]', with: ""
-    fill_in 'restaurant[restaurant_price]', with: "3"
-    fill_in 'restaurant[restaurant_address]', with: "Waterloo Station"
-    click_button 'Save Restaurant'
+    create_restaurant("Arnold's Chicken Hut", "", "3", "Paddington Station")
     visit('restaurants/')
     expect(page).not_to have_content("Arnold's Chicken Hut")
   end
 
+  scenario "restaurants/new should have a form" do
+    click_link 'Add restaurant'
+    expect(page)
+      .to have_field("restaurant[restaurant_name]")
+      .and have_field("restaurant[restaurant_cuisine]")
+      .and have_field("restaurant[restaurant_price]")
+      .and have_field("restaurant[restaurant_address]")
+  end
+
   scenario "failing to enter correct details should display a relative error" do
     click_link 'Add restaurant'
-    fill_in 'restaurant[restaurant_name]', with: "itsu"
-    fill_in 'restaurant[restaurant_cuisine]', with: "sushi"
-    fill_in 'restaurant[restaurant_price]', with: "3"
-    fill_in 'restaurant[restaurant_address]', with: "Waterloo Station"
-    click_button 'Save Restaurant'
-    visit('restaurants/')
-    expect(page).not_to have_content("Restaurant name is too short (minimum is 5 characters)")
+    create_restaurant("itsu", "sushi", "3", "Embankment")
+    expect(page).to have_content("Restaurant name is too short (minimum is 5 characters)")
+  end
+
+  scenario "Can edit a restaurant successfully" do
+    click_link 'Add restaurant'
+    create_restaurant("Riverwood Lotus", "Fresh", "3", "Lake district")
+    expect(page).to have_content("Riverwood Lotus")
+    click_link 'Edit'
+    update_restaurant("The hungry Caterpillar", "Fresh", "3", "Lake district")
+    expect(page).not_to have_content("Riverwood Lotus")
+    expect(page).to have_content("The hungry Caterpillar")
   end
 
 end
