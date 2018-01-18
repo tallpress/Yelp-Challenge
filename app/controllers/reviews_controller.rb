229683b15b20e0d267b3fb5a5942a90a4d9182c9
Valentina <valentina.rmo@gmail.com>
2018-01-18 10:35:25 +0000

    refactoring

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,8 +4,8 @@ class ReviewsController < ApplicationController
 
   def create
     @restaurant = Restaurant.find(params[:restaurant_id])
-    @user = current_user
-    # @review = @restaurant.reviews.create(review_params.merge(user: current_user)) #I am creating a new object and also saving it directly to the DB, I need to merge the info about the user because it doesn't know it by default
+
+     # @review = @restaurant.reviews.create(review_params.merge(user: current_user)) #I am creating a new object and also saving it directly to the DB, I need to merge the info about the user because it doesn't know it by default
 
     @review = @restaurant.reviews.new(review_params)  #a new active record object is created
     @review.user = current_user #the review is associated to an user
